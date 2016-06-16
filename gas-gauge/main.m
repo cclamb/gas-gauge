3a9fb15d7c9f7ea94e72661410af832340cc1f25
cclamb <cclamb@sandia.gov>
2016-06-16 10:06:39 -0600

    Initial Commit

---
+++ gas-gauge/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  gas-gauge
+//
+//  Created by Lamb, Christopher Charles on 6/16/16.
+//  Copyright © 2016 Lamb, Christopher Charles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
